6ed83cfb149736e4403514b012c997aa2b61b9fc
Skyagencia <skyagenciadgt@gmail.com>
2026-02-08 22:08:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Contabils PWA - sw.js
    ========================= */
 
-const VERSION = "V1.3"; // 🔁 TROQUE ISSO A CADA DEPLOY
+const VERSION = "V1.2"; // 🔁 TROQUE ISSO A CADA DEPLOY
 const CACHE_NAME = `contabils-cache-${VERSION}`;
 
 const APP_SHELL = [
@@ -101,5 +101,6 @@ self.addEventListener("fetch", (event) => {
     })()
   );
 });
+
 
 
